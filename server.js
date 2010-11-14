d49b4dac925331267c3c6b364510cb640417bdab
MARTIN_BROCHHAUS <MARTIN.BROCHHAUS@ORBIT.DE>
2010-11-14 15:13:34 +0800

    refactoring of dummy helper

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var connect = require('connect');
 var assetManager = require('connect-assetmanager');
 var assetHandler = require('connect-assetmanager-handlers');
 var express = require('express');
-var DummyHelper = require('./lib/dummy-helper');
+var dummyHelper = require('./lib/dummy-helper');
 var SocketServer = require('./lib/socket-server');
 var fs = require('fs');
 var sass = require('sass');
@@ -108,18 +108,31 @@ app.dynamicHelpers({
 	}
 });
 
-// Examples
-app.get('/your-page/', function(req, res) {
-	res.render('index', { locals: { 'key': 'value' } });
+//setup the errors
+app.error(function (err, req, res, next) {
+	console.log(err.stack.split('\n'));
+	res.render('500', {locals: {'err': err}});
 });
 
-app.post(/some-path/, function(req, res) {
-	console.log(req.body);
-	res.render('index', { locals: { 'key': 'value' } });
+//A route for creating a 500 error (useful to keep around for testing the page)
+app.get('/500', function (req, res) {
+    throw new Error('This is a 500 Error');
+});
+
+// Your routes
+app.get('/', function(req, res) {
+	var locals = { 'key': 'value' };
+	locals = dummyHelper.add_overlay(app, req, locals);
+	res.render('index', locals);
 });
 
 // Keep this just above .listen()
-var dummyTimestamps = new DummyHelper(app);
+var dummyTimestamps = new dummyHelper.DummyHelper(app);
+
+//The 404 route (ALWAYS keep this as the last route)
+app.get('/*', function (req, res) {
+    res.render('404');
+});
 
 app.listen(port, null);
 new SocketServer(app);
